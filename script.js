110d23148a254f1028a0bd40a55156384406c06a
Roman Ivannikov <roman.y.ivannikov@gmail.com>
2021-02-18 16:07:22 +0700

    build: lesson 14 complicated

--- script.js
+++ script.js
@@ -1,40 +1,87 @@
 'use strict';
 
 const DomElement = function(selector, height, width, bg, fontSize) {
-    this.selector = selector.trim();
-    this.height = height.trim();
-    this.width = width.trim();
-    this.bg = bg.trim();
-    this.fontSize = fontSize.trim();
+    this.selector = selector;
+    this.height = height;
+    this.width = width;
+    this.bg = bg;
+    this.fontSize = fontSize;
+};
+
+DomElement.prototype.getStyle = function() {
+    let cssInner = ``;
+    if (this.height !== undefined) {
+        cssInner += `height: ${this.height};`;
+    }
+    if (this.width !== undefined) {
+        cssInner += `width: ${this.width};`;
+    }
+    if (this.bg !== undefined) {
+        cssInner += `background-color: ${this.bg};`;
+    }
+    if (this.fontSize !== undefined) {
+        cssInner += `font-size: ${this.fontSize};`;
+    }
+    if (this.position !== undefined) {
+        cssInner += `position: ${this.position};`;
+    }
+    if (this.posX !== undefined) {
+        cssInner += `left: ${this.posX}px;`;
+    }
+    if (this.posY !== undefined) {
+        cssInner += `top: ${this.posY}px;`;
+    }
+
+    return cssInner;
 };
 
 DomElement.prototype.addElement = function() {
-    let element = '';
+    let 
+        element = '',
+        value = this.selector.slice(1);
     if (this.selector[0] === '.') {
-        // Создаем div с классом
-        element = document.createElement('div');
+        if (document.querySelector(`div.${value}`)) {
+            element = document.querySelector(`div.${value}`);
+        } else {
+            element = document.createElement('div');
+        }
         element.className = this.selector.slice(1);
         element.innerText = `Это блок <DIV> с классом ${this.selector.slice(1)}`;
     }
     if (this.selector[0] === '#') {
-        // Создаем параграф с id
-        element = document.createElement('p');
+        if (document.querySelector(`p#${value}`)) {
+            element = document.querySelector(`p#${value}`);
+        } else {
+            element = document.createElement('p');
+        }
         element.id = this.selector.slice(1);
         element.innerText = `Это блок <P> с ID ${this.selector.slice(1)}`;
     }
-    element.style.cssText = `
-        height: ${this.height};
-        width: ${this.width};
-        background-color: ${this.bg};
-        font-size: ${this.fontSize};
-    `;
+    
+    element.style.cssText = this.getStyle();
     document.body.append(element);
 };
 
-const newElementDiv = new DomElement('.class1', '100px', '500px', '#ccc', '14px');
-const newElementP = new DomElement('#id', '200px', '300px', '#333', '18px');
+const square = new DomElement('#square', '100px', '100px', '#ccc');
+square.position = 'absolute';
+square.posX = 0;
+square.posY = 0;
 
-console.dir(newElementDiv);
-
-newElementDiv.addElement();
-newElementP.addElement();
+document.addEventListener('keydown', function(event) {
+    switch (event.key) {
+        case 'ArrowLeft':
+            this.posX -= 10;
+            break;
+        case 'ArrowRight':
+            this.posX += 10;
+            break;
+        case 'ArrowUp':
+            this.posY -= 10;
+            break;
+        case 'ArrowDown':
+            this.posY += 10;
+            break;
+    }
+    this.addElement();
+}.bind(square));
+document.addEventListener('DOMContentLoaded', square.addElement.bind(square));
